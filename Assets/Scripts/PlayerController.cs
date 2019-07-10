4b11cdc4afac9e57e67c5a2537a79fd55ec32163
Nabil Sekirime <nabil@soulcadeinteractive.com>
2019-07-09 23:18:55 -0400

    Challenge #1 Adjustments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -46,10 +46,10 @@ public class PlayerController : MonoBehaviour
 
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
+        countText.text = "Collectables obtained: " + count.ToString();
         if (count >= 17)
         {
-            winText.text = "You Win!";
+            winText.text = "You have won!";
         }
     }
 }
